4778ab457a958dd83f5c4e893c51de8a35d114fe
Gajus <gajus@gajus.com>
2024-02-08 12:30:21 -0600

    clean up

--- app/server.js
+++ app/server.js
@@ -5,16 +5,10 @@ import { setTimeout as delay } from 'timers/promises';
 
 register("import-in-the-middle/hook.mjs", pathToFileURL("./"));
 
-const main = async () => {
-  await createSentry();
+await createSentry();
 
-  await delay(1000);
+// We need this because we have to guarantee that tracing instrumentation
+// is initialized before we import any modules that need to be instrumented.
+const { startServer } = await import('./startServer.js');
 
-  // We need this because we have to guarantee that tracing instrumentation
-  // is initialized before we import any modules that need to be instrumented.
-  const { startServer } = await import('./startServer.js');
-
-  await startServer();
-};
-
-main();
+await startServer();
