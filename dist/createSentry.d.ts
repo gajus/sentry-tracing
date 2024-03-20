b73b18665b4b6eb395b2b2e0e2a15e5f45dad687
Gajus <gajus@gajus.com>
2024-03-20 10:00:53 -0600

    wip

--- dist/createSentry.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const createSentry: () => void;
-//# sourceMappingURL=createSentry.d.ts.map
--- dist/startServer.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const startServer: () => Promise<void>;
-//# sourceMappingURL=startServer.d.ts.map
